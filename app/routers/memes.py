4361764d2e1ffd49ef0834a5911b10eb160d034b
Timur Usmanov <fonuatimur@gmail.com>
2024-06-12 15:00:15 +0700

    rework routing, rework schemas to correspond to new routing

--- app/routers/memes.py
+++ app/routers/memes.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, UploadFile, File, Form
+import httpx
 from sqlalchemy.orm import Session
 from typing import List
 from .. import crud, schemas, database
@@ -21,12 +22,32 @@ def read_meme(meme_id: int, db: Session = Depends(database.get_db)):
     return db_meme
 
 @router.post("/", response_model=schemas.Meme)
-def create_meme(meme: schemas.MemeBase, db: Session = Depends(database.get_db)):
+async def create_meme(title: str = Form(...),  description: str = Form(...), image_file: UploadFile = File(...), db: Session = Depends(database.get_db)):
+    async with httpx.AsyncClient() as client:
+        files = {'file':(image_file.filename, image_file.file, image_file.content_type)}
+        response = await client.post("http://media:8001/upload", files=files)
+
+        if response.status_code != 200:
+            raise HTTPException(status_code=500, detail="Failed to upload image")
+        
+        image_url = response.json().get("filename")
+    
+    meme = schemas.MemeUpdate(title=title, description=description, image_url=image_url)
     return crud.create_meme(db=db, meme=meme)
 
 @router.put("/{meme_id}", response_model=schemas.Meme)
-def update_meme(meme_id: int, meme: schemas.MemeBase, db: Session = Depends(database.get_db)):
-    return crud.update_meme(db=db, meme_id=meme_id, meme=meme)
+async def update_meme(meme_id: int, title: str = Form(...), description: str = Form(...), image_file: UploadFile = File(...), db: Session = Depends(database.get_db)):
+    async with httpx.AsyncClient() as client:
+        files = {'file':(image_file.filename, image_file.file, image_file.content_type)}
+        response = await client.post("http://media:8001/upload", files=files)
+
+        if response.status_code != 200:
+            raise HTTPException(status_code=500, detail="Failed to upload image")
+        
+        image_url = response.json().get("filename")
+    
+    meme = schemas.MemeUpdate(title=title, description=description, image_url=image_url)
+    return crud.update_meme(db=db, meme_id=meme_id, meme = meme)
 
 @router.delete("/{meme_id}", response_model=schemas.Meme)
 def delete_meme(meme_id: int, db: Session = Depends(database.get_db)):
--- app/schemas.py
+++ app/schemas.py
@@ -1,11 +1,18 @@
+from typing import Optional
 from pydantic import BaseModel
+from fastapi import UploadFile, File
 
 class MemeBase(BaseModel):
     title: str
     description: str
-    image_url: str
 
-class Meme(MemeBase):
+class MemeCreate(MemeBase):
+    pass
+
+class MemeUpdate(MemeBase):
+    image_url: Optional[str] = None
+
+class Meme(MemeUpdate):
     id: int
 
     class Config:
