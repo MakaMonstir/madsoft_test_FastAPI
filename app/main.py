351811e5e51714191465a5f5cd06beae6f7302da
Timur Usmanov <fonuatimur@gmail.com>
2024-06-12 11:02:19 +0700

    rework main: include routers to main, remove obsolele crud functionality

--- app/main.py
+++ app/main.py
@@ -1,31 +1,15 @@
-import ptvsd
 import uvicorn
 from fastapi import FastAPI
+from .routers import memes
+from .database import init_db
 
-ptvsd.enable_attach(address=('0.0.0.0', 5678), redirect_output=True)
+app = FastAPI()
 
-app = FastAPI(title='Memes App')
+app.include_router(memes.router)
 
-@app.get('/memes')
-def list_memes():
-    raise NotImplementedError
-
-@app.get('/memes/{meme_id}')
-def get_meme(meme_id: int):
-    raise NotImplementedError
-
-@app.post('/memes')
-def post_meme(image, text: str):
-    raise NotImplementedError
-
-@app.put('/memes/{meme_id}')
-def put_meme(meme_id: int):
-    raise NotImplementedError
-
-@app.delete('/memes/{meme_id}')
-def delete_meme(meme_id: int):
-    raise NotImplementedError
-
+@app.on_event("startup")
+def on_startup():
+    init_db()
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
